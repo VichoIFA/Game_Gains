bea9a040ce554c56704acb56db958accf4fcf85a
Vicente Fuentes <vicentedelaselva@gmail.com>
2024-11-29 22:15:25 -0300

    inicio del nuevo entorno virtual
    se creo un nuevo entorno virtual para trabajar en el nuevo proyecto

--- proyecto/GameGains/apps.py
+++ proyecto/GameGains/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class Mayorista9Config(AppConfig):
+class GamegainsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mayorista9'
+    name = 'GameGains'
--- proyecto/GameGains/urls.py
+++ proyecto/GameGains/urls.py
@@ -4,12 +4,4 @@ from . import views
 
 urlpatterns = [
     path('', views.Homepage, name='Homepage'),
-    path('Login', views.Login, name='Login'),
-    path('DashboardInicial/', views.Index,  name='DashboardInicial'),
-    path('detalleBarrasAgrupadas/<str:label>/<str:index>', views.BarrasAgrupadas, name='BarrasAgrupadas'),
-    path('ComprasCiudad/<str:ciudad>', views.ComprasCiudad, name='ComprasCiudad'),
-    path('genero/<str:genero>', views.Genero, name='genero'),
-    path('estadoCivil/<str:estadoCivil>', views.estadoCivil, name='estadoCivil'),
-    path('DashboardUsuarioCiudad/<str:ciudad>', views.DashboardUsuarioCiudad, name='DashboardUsuarioCiudad'),
-    path('profundizacion/<str:ciudad>/<str:producto>', views.Profundizacion, name='profundizacion')
 ]
