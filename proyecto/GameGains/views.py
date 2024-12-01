02b9183cb0db2cb37a7135448fd91e0a377043c4
Vicente Fuentes <vicentedelaselva@gmail.com>
2024-11-30 23:44:44 -0300

    desarrollo del dashboard del comprador y conexion a la base de datos
    la pagina del dashboard del comprador esta casi completa y se conecto a la base de datos

--- proyecto/GameGains/views.py
+++ proyecto/GameGains/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from . import models
 
 def Homepage(request):
     return render(request, 'Homepage.html')
@@ -9,13 +10,29 @@ def Login(request):
         password = request.POST.get('password', '')
 
         if username == 'admin' and password == '1234':
-            return redirect('DashboardInicial') 
+            return redirect('DashboardComprador.html') 
         if username == 'trabajadorCiudad_A' and password == 'ciudadA':
-            return redirect('DashboardUsuarioCiudad/A')
+            return redirect('DashboardComprador.html')
         if username == 'trabajadorCiudad_B' and password == 'ciudadB':
-            return redirect('DashboardUsuarioCiudad/B')        
+            return redirect('DashboardComprador.html')        
         if username == 'trabajadorCiudad_C' and password == 'ciudadC':
-            return redirect('DashboardUsuarioCiudad/B')
+            return redirect('DashboardComprador.html')
         else:
             return render(request, 'Login.html', {'error': 'Usuario o contraseña incorrectos'})
     return render(request, 'Login.html')
+
+def Comprador(request):
+    
+    TiposProductos = ["Espadas", "Armaduras", "Pociones", "Arcos", "Escudos", "Anillos", "Amuletos"]
+
+    PrecioPromedio = models.PrecioPromedioPorTipo(5)
+    StockTotal = models.StockEnMercadoTotal(5)
+    CantidadPorTipo = models.CantidadDisponiblePorTipo(5)
+    print(CantidadPorTipo)
+    
+    context = {
+        "PrecioPromedio" : PrecioPromedio,
+        "StockTotal" : StockTotal,
+        "CantidadPorTipo" : CantidadPorTipo
+    }
+    return render(request, 'DashboardComprador.html', context)
